165632bafcac0fcab3c5bda6396ab4ffa5dc35da
lestacik852 <m.lestak95@gmail.com>
2023-10-03 21:09:39 +0200

    doplnenie

---
+++ javascript/script.js
@@ -0,0 +1,28 @@
+function myFunctionToggle() {
+    const text = document.getElementById("demo-6");
+    const button = document.getElementById("button-toogle");
+
+    if (text.style.display === "none") {
+        button.className = "off";
+        text.style.display = "block";
+        button.innerText = "Schovať text!";
+    } else {
+        button.className = "on";
+        text.style.display = "none";
+        button.innerText = "Ukázať text!";
+    }
+}
+function myFunctionToggle() {
+    const text = document.getElementById("demo-6");
+    const button = document.getElementById("button-toogle");
+
+    if (text.style.display === "none") {
+        button.className = "off";
+        text.style.display = "block";
+        button.innerText = "Schovať text!";
+    } else {
+        button.className = "on";
+        text.style.display = "none";
+        button.innerText = "Ukázať text!";
+    }
+}
