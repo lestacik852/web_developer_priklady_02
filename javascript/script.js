a4e9908c0f9714e3dcb9c4318f5a213baa510866
lestacik852 <m.lestak95@gmail.com>
2023-10-03 22:25:18 +0200

    doplnenie

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,6 @@
-function myFunctionToggle() {
-    const text = document.getElementById("demo-6");
-    const button = document.getElementById("button-toogle");
-
-    if (text.style.display === "none") {
-        button.className = "off";
-        text.style.display = "block";
-        button.innerText = "Schovať text!";
-    } else {
-        button.className = "on";
-        text.style.display = "none";
-        button.innerText = "Ukázať text!";
-    }
+function myFunction() {
+    const text = document.getElementById("demo-3");
+    text.innerHTML = "Ahojky";
 }
 function myFunctionToggle() {
     const text = document.getElementById("demo-6");
@@ -19,10 +9,23 @@ function myFunctionToggle() {
     if (text.style.display === "none") {
         button.className = "off";
         text.style.display = "block";
-        button.innerText = "Schovať text!";
+        button.innerText = "Skryť text!";
     } else {
         button.className = "on";
         text.style.display = "none";
         button.innerText = "Ukázať text!";
     }
 }
+
+function formatDate() {
+    const date = new Date();
+    const day = date.getDate().toString();
+    const month = (date.getMonth() + 1).toString();
+    const year = date.getFullYear().toString();
+    const hour = date.getHours().toString();
+    const minute = date.getMinutes().toString();
+    const dateDisplay = document.getElementById("demo");
+
+    dateDisplay.innerHTML = hour + ":" + minute + "<br>" + day + "." + month + "." + year;
+}
+
